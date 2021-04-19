47986601be11f9bc7cdf17207e0a2e6c8375942f
Rafi Hanif <rafihanif86@gmail.com>
2021-04-19 12:22:38 +0700

    Service menggunakan looping timer

--- app/src/main/java/android/packt/com/androidwarehousemonitor/MyService.java
+++ app/src/main/java/android/packt/com/androidwarehousemonitor/MyService.java
@@ -13,6 +13,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MyService  extends Service {
     LogBook  logBook;
@@ -33,7 +35,11 @@ public class MyService  extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
         logBook =  (LogBook) intent.getSerializableExtra("logBook");
-        while (logBook.getLongitude() != 0 && logBook.getLatitude() >= 0) {
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+
+//        while (logBook.getLongitude() != 0 && logBook.getLatitude() >= 0) {
             logBook =  MainActivity.logBook;
 
             logBook.setTime(java.text.DateFormat.getDateTimeInstance().format(new Date()));
@@ -42,11 +48,14 @@ public class MyService  extends Service {
             DatabaseReference myRef = database.getReference("Log");
             myRef.push().setValue(logBook);
 
-            Toast.makeText(this, "Telah di upload", Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "Telah di upload", Toast.LENGTH_SHORT).show();
+//
+//            SystemClock.sleep(60000); // 30 seconds
+//
+//        }
+            }
 
-            SystemClock.sleep(60000); // 30 seconds
-
-        }
+        }, 60000, 60000);
         Toast.makeText(this, "End of Start", Toast.LENGTH_LONG).show();
         return START_STICKY;
     }
